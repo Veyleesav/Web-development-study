c4c93ba255101c7d824428e03773a2996e682cb0
Lev Vasilyev <wanbergg@gmail.com>
2018-06-23 16:10:44 +0300

    Course 3 week 5
    task

---
+++ Yandex&MIPT_Frontend/Course_3__Layout/week_5/site/scripts/scripts.js
@@ -0,0 +1,18 @@
+function maksim() {
+    document.getElementById('u153').style.backgroundColor = '#ffe05d';
+    document.getElementById('u531-6').style.visibility = 'visible';
+    document.getElementById('u528-6').style.visibility = 'hidden';
+    document.getElementById('u525-4').style.visibility = 'hidden';
+}
+function alexey() {
+    document.getElementById('u153').style.backgroundColor = '#ff8283';
+    document.getElementById('u531-6').style.visibility = 'hidden';
+    document.getElementById('u528-6').style.visibility = 'visible';
+    document.getElementById('u525-4').style.visibility = 'hidden';
+}
+function george() {
+    document.getElementById('u153').style.backgroundColor = '#72C4F6';
+    document.getElementById('u531-6').style.visibility = 'hidden';
+    document.getElementById('u528-6').style.visibility = 'hidden';
+    document.getElementById('u525-4').style.visibility = 'visible';
+}
